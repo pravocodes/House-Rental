14d431f71f3ce5edbbb60dfa4a07d3bb806c7db0
Mohak1301 <mohaktiwari2004@gmail.com>
2024-05-01 20:23:16 +0530

    2nd

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
-
+import Slide from '../component/Slide'
+import Categories from '../component/Categories'
 const HomePage = () => {
   return (
     
     <div>
       < Navbar />
-    
+      <Slide />
+      <Categories/>
     </div>
   )
 }
