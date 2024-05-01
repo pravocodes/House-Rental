0cf638e8b4054fb4368ad267fb5804480ba11165
Mohak1301 <mohaktiwari2004@gmail.com>
2024-05-01 16:31:30 +0530

    Navbar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../component/Navbar'
 
 const HomePage = () => {
   return (
+    
     <div>
-      Homepage
+      < Navbar />
+    
     </div>
   )
 }
